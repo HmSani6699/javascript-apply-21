2eb70778e438cd29b833d8b569d1ee0d82c2c75d
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-31 13:49:33 +0600

    ad array to sum in all odd number

--- array-sum.js
+++ array-sum.js
@@ -1,13 +1,35 @@
-//sum array number 
+//sum array to all event number 
 function sumNumber(num) {
     let sum = 0;
     for (var i = 0; i < num.length; i++) {
         let arrayElement = num[i];
-        sum += arrayElement;
+        if (arrayElement % 2 === 0) {
+            sum += arrayElement;
+        }
     }
     return sum;
 }
-var num = [20, 10, 30, 40, 50];
+var num = [20, 11, 30, 40, 50];
 var totalNumber = sumNumber(num);
 // console.log(totalNumber)
+
+
+
+//sum array to all odd number
+
+function sumOddNumber(num) {
+    let sum = 0;
+    for (var i = 0; i < num.length; i++) {
+        const index = i;
+        const element = num[index];
+        if (element % 2 != 0) {
+            sum += element;
+        }
+    }
+    return sum
+}
+
+const arry = [10, 33, 40, 20, 39, 48, 57];
+const sumOutput = sumOddNumber(arry);
+console.log(sumOutput)
 
