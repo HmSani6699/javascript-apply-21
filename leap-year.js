c98a80e15af82ed76ca1494cc2de7111cbd0387a
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-31 20:49:09 +0600

    ad to leap year part 2

--- leap-year.js
+++ leap-year.js
@@ -5,4 +5,20 @@ function isYearInLeapYear(year) {
     return 'is year in not leap year'
 }
 
-console.log(isYearInLeapYear(1900));
+// console.log(isYearInLeapYear(1900));
+
+
+
+function isLeapYear(year) {
+    if (year % 4 === 0) {
+        console.log('Year is Leap Year: ', year)
+    }
+    else {
+        console.log('Year is not Leap Year :', year)
+    }
+}
+isLeapYear(2001)
+
+
+
+
