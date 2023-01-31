406b6657ff8fed77147d7f9459211d33f019b852
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-01-31 09:47:47 +0600

    ad the inch to convert your centimeter

--- inchToFeet.js
+++ inchToFeet.js
@@ -5,3 +5,13 @@ function inchToFeet(inch) {
 const mamaInch = 150;
 const mamaFeet = inchToFeet(mamaInch);
 // console.log(mamaFeet);
+
+
+function inchToCentimeter(inch) {
+    var centimeter = inch * 2.54;
+    return centimeter;
+}
+
+var naniInch = 5;
+var naniCentimeter = inchToCentimeter(naniInch);
+console.log(naniCentimeter);
